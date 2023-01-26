b3bcbe003b6cea31250f6d05cb8ad1149dfb65ad
Mehul Agarwal <agarwalmehul92@gmail.com>
2023-01-26 12:38:55 +0530

    Change Logging in startServer

--- startServer.mjs
+++ startServer.mjs
@@ -1,9 +1,9 @@
-import logger from '@am92/api-logger'
 import ExpressUtils from '@am92/express-utils'
 
 import SERVER_CONFIG, { SERVICE } from './config/SERVER_CONFIG.mjs'
 
 const { PORT } = SERVER_CONFIG
+const successLogFunc = console.success || console.info
 
 const startServer = async (app) => {
   try {
@@ -12,10 +12,10 @@ const startServer = async (app) => {
 
     // Start Server
     await app.listen(PORT)
-    logger.success(`[${SERVICE}] Server Started Successfully! Listening on Port: ${PORT}`)
+    successLogFunc(`[${SERVICE}] Server Started Successfully! Listening on Port: ${PORT}`)
   } catch (error) {
     const errorMessage = `[${SERVICE}] ${error.message}`
-    logger.error(errorMessage, error)
+    console.error(errorMessage, error)
     throw error
   }
 }
